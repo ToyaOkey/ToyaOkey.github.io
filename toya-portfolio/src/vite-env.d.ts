d509be7b41b78145bd4d9107de464119d3b4c2c4
ToyaOkey <cooltoya12@gmail.com>
2025-11-07 00:25:50 -0500

    Updates

--- toya-portfolio/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-
-    ],
-})
